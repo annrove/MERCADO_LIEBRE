86a31e7a95cc611f5c92e4a6e34a35023ec8bf8b
annrove <annrove@gmail.com>
2022-10-10 00:42:47 -0500

    cargue de archivos iniciales

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const path = require('path');
+
+app.use(express.static("public"))
+
+app.listen(3030, () => console.log("Servidor en servicio: http://localhost:3030"));
+
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, './views/home.html'));
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(path.join(__dirname, './views/login.html'));
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(path.join(__dirname, './views/register.html'));
+});
